b8bc061525021a3bf7ab36e6e53f6af9cc84e245
Sean Farley <sean.michael.farley@gmail.com>
2013-11-08 19:49:36 -0600

    sentinel: fix incorrect error message if there is nothing to compile
    Previously, if a user ran latexmk and there was nothing to compile,
    then the error "LatexMk: problems after [0] pages" would appear.
    Instead, we now display "LatexMk: nothing to do".

--- auctex-latexmk.el
+++ auctex-latexmk.el
@@ -99,15 +99,17 @@
     (goto-char (point-max))
     (cond
       ((re-search-backward (format "^%s finished at" name) nil t)
-       (re-search-backward "^Run number [0-9]+ of rule '\\(pdf\\|lua\\|xe\\)?latex'" nil t)
-       (forward-line 5)
-       (let ((beg (point)))
-         (re-search-forward "^Latexmk:" nil t)
-         (beginning-of-line)
-         (save-restriction
-           (narrow-to-region beg (point))
-           (goto-char (point-min))
-           (TeX-LaTeX-sentinel process name))))
+       (if (re-search-backward "^Run number [0-9]+ of rule '\\(pdf\\|lua\\|xe\\)?latex'" nil t)
+           (progn
+             (forward-line 5)
+             (let ((beg (point)))
+               (re-search-forward "^Latexmk:" nil t)
+               (beginning-of-line)
+               (save-restriction
+                 (narrow-to-region beg (point))
+                 (goto-char (point-min))
+                 (TeX-LaTeX-sentinel process name))))
+         (message (format "%s: nothing to do" name))))
       ((re-search-backward (format "^%s exited abnormally with code" name) nil t)
        (re-search-backward "^Collected error summary (may duplicate other messages):" nil t)
        (re-search-forward "^  \\([^:]+\\):" nil t)
