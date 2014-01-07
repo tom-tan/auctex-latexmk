b542ab200c4cd5582b4b5fc1fe8f1a01fe37d3f6
Tomoya Tanjo <ttanjo@gmail.com>
2014-01-07 19:13:43 +0900

    Update copyright years

--- auctex-latexmk.el
+++ auctex-latexmk.el
@@ -1,6 +1,6 @@
 ;;; auctex-latexmk.el --- Add LatexMk support to AUCTeX
 
-;; Copyright (C) 2013 by Tomoya Tanjo
+;; Copyright (C) 2013, 2014 by Tomoya Tanjo
 
 ;; Author: Tomoya Tanjo <ttanjo@gmail.com>
 ;; URL: https://github.com/tom-tan/auctex-latexmk/
